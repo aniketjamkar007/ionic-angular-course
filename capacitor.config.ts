b9f1c3f6e5e599ac3efe772e957d56fff4bf4e4e
aniketjamkar007 <aniketjamkar@gmail.com>
2022-08-05 16:00:53 +0530

    android build changes

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.developer.ionicapp',
   appName: 'ionic-angular-course',
   webDir: 'www',
   bundledWebRuntime: false
